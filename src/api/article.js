1bc45acbfbcf856ed4660f98788a08913fe79396
Mac <279222055@qq.com>
2022-05-30 10:41:23 +0800

    测试开发部署

--- src/api/article.js
+++ src/api/article.js
@@ -8,13 +8,7 @@ export function fetchList(query) {
   })
 }
 
-export function fetchArticle(id) {
-  return request({
-    url: '/vue-element-admin/article/detail',
-    method: 'get',
-    params: { id }
-  })
-}
+
 
 
 
