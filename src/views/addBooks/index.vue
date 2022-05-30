285e8b35dd5e609099e785cd9b85904fb5d07229
Mac <279222055@qq.com>
2022-05-30 16:26:50 +0800

    修改标识问题

--- src/views/addBooks/index.vue
+++ src/views/addBooks/index.vue
@@ -56,7 +56,7 @@
                             >
                                 <i class="el-icon-upload"></i>
                                 <div class="el-upload__text">将文件拖到此处，或<em>点击上传</em></div>
-                                <div class="el-upload__tip" slot="tip">只能上传jpg/png文件，且不超过5MB</div>
+                                <div class="el-upload__tip" slot="tip">上传书本HMLT文件，且不超过5MB</div>
                             </el-upload>
                     </el-form-item>
                 </el-form>
