c000a63db56dfeb8e6a66c9c977a8555aa19e812
Mac <279222055@qq.com>
2022-05-30 13:07:46 +0800

    测试

--- src/api/role.js
+++ src/api/role.js
@@ -1,11 +1,5 @@
 import request from '@/utils/request'
 
-export function getRoutes() {
-  return request({
-    url: '/vue-element-admin/routes',
-    method: 'get'
-  })
-}
 
 export function getRoles() {
   return request({
