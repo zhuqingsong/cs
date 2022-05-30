94769084adb55f0f7f2499313c194aa9bdcd21e9
Mac <279222055@qq.com>
2022-05-30 13:51:52 +0800

    修改配置文件

--- vue.config.js
+++ vue.config.js
@@ -25,7 +25,7 @@ module.exports = {
    * Detail: https://cli.vuejs.org/config/#publicpath
    */
   publicPath: '/',
-  outputDir: 'dist',
+  outputDir: 'admin',
   assetsDir: 'static',
   lintOnSave: false,
   productionSourceMap: false,
